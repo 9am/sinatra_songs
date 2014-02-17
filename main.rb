e4c9012766383e667ba8114a0315717bf42c3bd6
GBXXX <gaobo.xxx@gmail.com>
2014-02-17 15:56:58 +0800

    update code from sitepoint

--- main.rb
+++ main.rb
@@ -3,20 +3,38 @@ require 'slim'
 require 'sass'
 require './song'
 
+configure do
+  enable :sessions
+  set :username, 'frank'
+  set :password, 'sinatra'
+end
+
 configure :development do
   DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
 end
+
 configure :production do
   DataMapper.setup(:default, ENV['DATABASE_URL'])
 end
 
-configure do
-  enable :sessions
-  set :username, 'eric'
-  set :password, 'sinatra'
-  get('/styles.css'){ scss :styles }
+get('/styles.css'){ scss :styles }
+
+get '/' do
+  slim :home
 end
 
+get '/about' do
+  @title = "All About This Website"
+  slim :about
+end
+
+get '/contact' do
+  slim :contact
+end
+
+not_found do
+  slim :not_found
+end
 
 get '/login' do
   slim :login
@@ -36,55 +54,10 @@ get '/logout' do
   redirect to('/login')
 end
 
-
-get '/' do
-  slim :home
+get '/set/:name' do
+  session[:name] = params[:name]
 end
 
-get '/about' do
-  @title = "All About This Website"
-  slim :about
-end
-
-get '/contact' do
-  slim :contact
-end
-
-
-
-get '/songs' do
-  halt(401,'Not Authorized') unless session[:admin]
-  @songs = Song.all
-  slim :songs
-end
-
-post '/songs' do
-  song = Song.create(params[:song])
-  redirect to("/songs/#{song.id}")
-end
-
-get '/songs/new' do
-  @song = Song.new
-  slim :new_song
-end
-
-get '/songs/:id' do
-  @song = Song.get(params[:id])
-  slim :show_song
-end
-
-get '/songs/:id/edit' do
-  @song = Song.get(params[:id])
-  slim :edit_song
-end
-
-put '/songs/:id' do
-  song = Song.get(params[:id])
-  song.update(params[:song])
-  redirect to("/songs/#{song.id}")
-end
-
-delete '/songs/:id' do
-  Song.get(params[:id]).destroy
-  redirect to('/songs')
+get '/get/hello' do
+  "Hello #{session[:name]}"
 end
